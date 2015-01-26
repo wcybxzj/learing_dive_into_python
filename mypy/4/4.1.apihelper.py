b2cc004300e67673d4e0bcb303611713dd48aaa6
wcybxzj <virgo86ybx@gmail.com>
2015-01-27 03:26:13 +0800

    3 done working on 4

---
+++ mypy/4/4.1.apihelper.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def info(object, spacing=10, collapse=1):
+    """Print methods and doc strings.
+
+    Takes module, class, list, dictionary, or string."""
+    methodList=[method for method in dir(object) \
+                    if callable(getattr(object, method))]
+
+    processFunc = collapse and \
+            ( lambda s:" ".join( s.split() ) )  or \
+            (lambda s: s)
+
+    print "\n".join(["%s %s" % \
+            (method.ljust(spacing),
+                processFunc(str(getattr(object, method).__doc__))
+                )
+            for method in methodList])
+
+
+class myclass(object):
+    """docstring for myclass"""
+    def fname(self):
+        """0000000000
+
+        1111111111111"""
+        pass
+
+if __name__ == '__main__':
+    li = []
+    info(li, 30)
+    print '==========================='
+    info(li, 30, 0)
+    print '==========================='
+    my_obj = myclass()
+    info(my_obj, 30, 0)
