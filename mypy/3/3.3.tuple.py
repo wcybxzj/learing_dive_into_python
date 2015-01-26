cd6ba6c692b9c164f75f4022df0e74b6d2897797
ybx <87212426@qq.com>
2015-01-26 16:12:05 +0800

    3.3

---
+++ mypy/3/3.3.tuple.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+t = ('a', 'b', 'mpilgrim', 'z', 'example')
+
+#1.tuple can not append
+try:
+    t.append("new")
+except AttributeError:
+    print 'tuple can not append'
+
+#2.typle change to list
+t_list = list(t)
+t_list.append("1111")
+print t_list
+
+#3.in
+if "z" in t:
+    print 'have z in this tuple'
+
+#4.tuple做list的key
+list = {t:'121212'}
+print list[t]
