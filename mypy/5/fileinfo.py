eb0360f1874c503231574af1941d3c7282e43ca1
wcybxzj <virgo86ybx@gmail.com>
2015-01-30 12:56:49 +0800

    mothing change

--- mypy/5/fileinfo.py
+++ mypy/5/fileinfo.py
@@ -4,28 +4,28 @@ import os, sys
 
 
 class FileInfo(object):
-	"""docstring for FileInfo"""
-	def __init__(self, arg):
-		super(FileInfo, self).__init__()
-		self.arg = arg
+    """docstring for FileInfo"""
+    def __init__(self, arg):
+        super(FileInfo, self).__init__()
+            self.arg = arg
 
 def listDirectory(directory, fileExtList):
-	""" get list of file info obj for file of particular extentsions"""
-	fileList = [os.path.normcase(f)
-			for f in os.listdir(directory)]
-	fileList = [ os.path.join(directory, f)
-			for f in fileList
-			if os.path.splitext(f)[1] in fileExtList ]
-	#print fileList
-	def getFileInfoClass(filename, module=sys.modules[FileInfo.__module__]):
-		"""docstring for getFileInfoClass"""
-		subclass = "%sFileInfo" % os.path.splitext(filename)[1].upper()[1:]
-		return hasattr(module, subclass) and getattr(module, subclass) or FileInfo
-	return [getFileInfoClass(f)(f) for f in fileList]
+    """ get list of file info obj for file of particular extentsions"""
+    fileList = [os.path.normcase(f)
+            for f in os.listdir(directory)]
+    fileList = [ os.path.join(directory, f)
+            for f in fileList
+            if os.path.splitext(f)[1] in fileExtList ]
+    #print fileList
+    def getFileInfoClass(filename, module=sys.modules[FileInfo.__module__]):
+        """docstring for getFileInfoClass"""
+            subclass = "%sFileInfo" % os.path.splitext(filename)[1].upper()[1:]
+            return hasattr(module, subclass) and getattr(module, subclass) or FileInfo
+    return [getFileInfoClass(f)(f) for f in fileList]
 
 def main():
-	project_path = os.path.realpath('.')
-	listDirectory(project_path, [".py"])
+    project_path = os.path.realpath('.')
+    listDirectory(project_path, [".py"])
 
 if __name__ == '__main__':
-	main()
+    main()
